8a2d4fe328bb8f74bf8499ec3912b47a9cab1f7a
Deepak Balaji <147257487+Deepak-Balaji07@users.noreply.github.com>
2025-03-31 21:49:36 +0530

    Time: 2 ms (12.3%), Space: 41.8 MB (64.88%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int count=0;
+        String x = s.trim();
+        for(int i=0;i<x.length();i++){
+            if(x.charAt(i)==' '){
+                count=0;
+            }
+            else{
+                count++;
+            }
+        }
+        return count; 
+    }
+}
