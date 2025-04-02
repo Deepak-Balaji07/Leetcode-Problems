8144510d711800edaa1a11287e1c94c7373801a5
Deepak Balaji <147257487+Deepak-Balaji07@users.noreply.github.com>
2025-04-02 21:40:01 +0530

    Time: 4 ms (97.27%), Space: 45.1 MB (8.05%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x =0,y=0;
+        for(char c:moves.toCharArray()){
+            switch(c){
+                case 'R': x+=1; break;
+                case 'L': x-=1; break;
+                case 'U': y+=1; break;
+                case 'D': y-=1; break;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
